19a2c8b604cf711d5992d761c8e0de396cb24726
razorhedge <mat@stgolab.com>
2022-10-12 15:29:48 -0300

    Updated Analysis

---
+++ Scripts/model.py
@@ -0,0 +1,12 @@
+from sklearn.model_selection import train_test_split, cross_val_score, RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, recall_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.dummy import DummyClassifier
+from xgboost import XGBClassifier
+
+class Model():
+    def __init__(self) -> None:
+        pass
+
+    
